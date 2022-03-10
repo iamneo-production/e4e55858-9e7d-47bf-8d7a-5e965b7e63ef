5079e6adb610ae683cd7a0efca2ba11bf5b96e6c
patrasomu <patrasomnath055@gmail.com>
2022-03-10 10:21:53 +0000

    sprint1 model setup

---
+++ springapp/src/main/java/com/examly/springapp/model/JwtRequest.java
@@ -0,0 +1,29 @@
+package com.examly.springapp.model;
+
+public class JwtRequest {
+
+	 String username;
+	 String password;
+	public String getUsername() {
+		return this.username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public JwtRequest(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public JwtRequest() {
+		
+	}
+	
+	
+}
