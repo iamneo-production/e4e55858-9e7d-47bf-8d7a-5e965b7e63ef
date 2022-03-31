dcdd3124d4a6f507de2c2863a153921b0cb7a751
SHASHWAT KUMAR SHRIVASTAVA <69300179+Shashwat05-shrivastava@users.noreply.github.com>
2022-03-31 11:51:33 +0530

    Update reviews.component.ts

--- angularapp/src/app/pages/reviews/reviews.component.ts
+++ angularapp/src/app/pages/reviews/reviews.component.ts
@@ -7,17 +7,17 @@ import { ReviewService } from 'src/app/services/review.service';
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css']
 })
-export class ReviewsComponent implements OnInit {
+export class ReviewsComponent implements OnInit {  
 
   constructor(private reviewservice:ReviewService,private route:ActivatedRoute) { }
-  reviews;
-  instituteId;
-
-  ngOnInit(): void {
-    this.instituteId=this.route.snapshot.params['instituteid'];
-    this.reviewservice.getallReviews(this.instituteId).subscribe((data:any)=>{
-      this.reviews=data;
-      console.log(data)
+  reviews; 
+  instituteId;  
+  // routing of reviews  
+  ngOnInit(): void {  
+    this.instituteId=this.route.snapshot.params['instituteid'];  
+    this.reviewservice.getallReviews(this.instituteId).subscribe((data:any)=>{ 
+      this.reviews=data;  
+      console.log(data)  
     })
 
   }
