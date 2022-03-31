28db3811e9590337305b23cd64d51d94dcd7b249
gopal78938 <78066145+gopal78938@users.noreply.github.com>
2022-03-31 12:49:02 +0530

    Update addacademy.component.ts

--- angularapp/src/app/pages/admin/addacademy/addacademy.component.ts
+++ angularapp/src/app/pages/admin/addacademy/addacademy.component.ts
@@ -17,107 +17,128 @@ import Swal from 'sweetalert2';
 
 export class AddacademyComponent implements OnInit { 
 
- 
+  
   constructor(private instituteService: InstituteService,private snack: MatSnackBar,private router:Router) { } 
 
-  ngOnInit(): void {  
-  }  
+  ngOnInit(): void {   
+  }   
 
-  IDExist=""  
-  
-
-  institute={  
-    instituteId:'', 
-    instituteName:'', 
-    mobile:'', 
-    email:'', 
-    instituteDescription:'', 
-    imageUrl:'', 
-    instituteAddress:'' 
-  }; 
-
-//onSubmit method is used
-  
-  onSubmit() { 
+  IDExist=""   
+   
+ 
+  institute={   
+    instituteId:'',  
+    instituteName:'',  
+    mobile:'',  
+    email:'',  
     
-    //check whether 
+    instituteDescription:'',  
+    imageUrl:'',  
+    instituteAddress:''  
+  };  
+ 
+//onSubmit method is used   
+     
+  onSubmit() {  
+           
+    //check whether    
+     
+    if (this.institute.instituteId == '' || this.institute.instituteId == null) {  
+       
+      this.snack.open("Email is required !! ", '', {  
+        duration: 3000,  
+        
+        // verticalPosition: 'top',   
+        // horizontalPosition: 'right',  
+        
+      });  
+      return;  
+    }      
+    if (this.institute.email == '' || this.institute.email == null) {  
+      this.snack.open("Email is required !! ", '', {  
+        duration: 3000,  
+        // verticalPosition: 'top',    
+        // horizontalPosition: 'right',   
+      });      
+      
+        
+      return;   
+    }  
     
-    if (this.institute.instituteId == '' || this.institute.instituteId == null) { 
-      this.snack.open("Email is required !! ", '', { 
-        duration: 3000, 
-        // verticalPosition: 'top', 
-        // horizontalPosition: 'right', 
+    if (this.institute.instituteName.trim() == '' || this.institute.instituteName == null) {   
+      this.snack.open("institutename is required !! ", '', {    
+        duration: 3000,      
+      });       
+      
+      return;      
+    } 
+    
+    if(this.institute.mobile == '' || this.institute.mobile== null) { 
+      this.snack.open("Mobile Number is required !! ", '', {  
+        duration: 3000,  
       }); 
+      
       return; 
     } 
-    if (this.institute.email == '' || this.institute.email == null) { 
-      this.snack.open("Email is required !! ", '', { 
-        duration: 3000, 
-        // verticalPosition: 'top', 
-        // horizontalPosition: 'right', 
-      }); 
-      return; 
-    } 
-    if (this.institute.instituteName.trim() == '' || this.institute.instituteName == null) { 
-      this.snack.open("institutename is required !! ", '', { 
-        duration: 3000, 
-      }); 
-      return; 
-    } 
-    if (this.institute.mobile == '' || this.institute.mobile== null) { 
-      this.snack.open("Mobile Number is required !! ", '', { 
-        duration: 3000, 
-      }); 
-      return; 
-    } 
-    if (this.institute.instituteAddress == '' || this.institute.instituteAddress== null) { 
-      this.snack.open("Mobile Number is required !! ", '', { 
-        duration: 3000, 
-      }); 
+    
+    if (this.institute.instituteAddress == '' || this.institute.instituteAddress== null) {  
+      this.snack.open("Mobile Number is required !! ", '', {  
+        duration: 3000,  
+      });  
+      
       return; 
     } 
     if (this.institute.instituteDescription == '' || this.institute.instituteDescription== null) { 
       this.snack.open("Mobile Number is required !! ", '', { 
         duration: 3000, 
-      }); 
-      return; 
-    } 
-    
-      this.instituteService.addInstitute(this.institute).subscribe((data:any)=> 
-      { 
+      });  
+      return;  
+    }  
+     
+      this.instituteService.addInstitute(this.institute).subscribe((data:any)=>  
+      {  
+          
+         Swal.fire('Successfully Added !!', 'Institute Name : ' + data.instituteName, 'success');  
+         this.router.navigate(['admin/institutes']);  
+      },  
+                                                                   
+      (error) => {  
          
-         Swal.fire('Successfully Added !!', 'Institute Name : ' + data.instituteName, 'success'); 
-         this.router.navigate(['admin/institutes']); 
-      }, 
-      (error) => { 
-        
-        console.log("mes"); 
-        console.log(error.message) 
+        console.log("mes");   
+        console.log(error.message)  
+         
         //alert('Something Wrong'); 
-        this.snack.open(error.error.message, '', { 
-          duration: 5000, 
-        }); 
+        this.snack.open(error.error.message, '', {  
+          duration: 5000,  
+        });  
+        
+      });  
        
-      }); 
-    
-  } 
-
-
-  checkId(){ 
-      console.log("calle") 
-    this.instituteService.checkInstituteId(this.institute.instituteId).subscribe((data)=> 
-    { 
-      this.IDExist=''; 
-    }, 
-    (error) => { 
-      this.IDExist=error.error.message; 
-     console.log(error); 
-     this.ngOnInit(); 
-      //alert('Something Wrong'); 
+  }     
+     
+ 
+  checkId(){  
+      console.log("calle")   
+    this.instituteService.checkInstituteId(this.institute.instituteId).subscribe((data)=>  
+    {  
+      this.IDExist='';  
+    },   
+                                                                                  
+    (error) => {   
+      this.IDExist=error.error.message;    
+     console.log(error);     
+     this.ngOnInit();  
       
-    }) 
-  } 
-
-
+       
+      //alert('Something Wrong');    
+             
+    })   
+     
+     
+  }   
+ 
   
+     
+   
+     
 } 
