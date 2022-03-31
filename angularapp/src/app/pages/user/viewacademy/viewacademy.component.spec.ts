ec9ccd41aefc601879f15d11c6eb307eb0efc75c
premkumarpaul <55930528+premkumarpaul@users.noreply.github.com>
2022-03-31 11:45:47 +0530

    Update viewacademy.component.spec.ts

--- angularapp/src/app/pages/user/viewacademy/viewacademy.component.spec.ts
+++ angularapp/src/app/pages/user/viewacademy/viewacademy.component.spec.ts
@@ -1,25 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; 
+ 
 
-import { ViewacademyComponent } from './viewacademy.component';
+import { ViewacademyComponent } from './viewacademy.component'; 
 
-describe('ViewacademyComponent', () => {
-  let component: ViewacademyComponent;
-  let fixture: ComponentFixture<ViewacademyComponent>;
 
-  beforeEach(async () => {
+describe('ViewacademyComponent', () => { 
+  
+  let component: ViewacademyComponent; 
+  
+  let fixture: ComponentFixture<ViewacademyComponent>; 
+  
+  
+
+  beforeEach(async () => { 
+    
     await TestBed.configureTestingModule({
-      declarations: [ ViewacademyComponent ]
-    })
-    .compileComponents();
-  });
+      
+      declarations: [ ViewacademyComponent ] 
+       
+    })  
+    .compileComponents();  
+  });  
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ViewacademyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => { 
+    fixture = TestBed.createComponent(ViewacademyComponent); 
+    component = fixture.componentInstance;  
+    fixture.detectChanges();  
+  });  
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should create', () => {  
+    expect(component).toBeTruthy();  
+  });  
+});  
+ 
