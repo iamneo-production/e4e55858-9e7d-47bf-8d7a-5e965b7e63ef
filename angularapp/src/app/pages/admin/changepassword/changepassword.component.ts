7b324aab4b4ff7d241995fa357dc2fe6ff091004
Pranav-11-ctr <75959104+Pranav-11-ctr@users.noreply.github.com>
2022-03-31 12:53:57 +0530

    Update changepassword.component.ts

--- angularapp/src/app/pages/admin/changepassword/changepassword.component.ts
+++ angularapp/src/app/pages/admin/changepassword/changepassword.component.ts
@@ -4,44 +4,44 @@ import { Router } from '@angular/router';
 import { UserService } from 'src/app/user.service';
 import Swal from 'sweetalert2';
 
-@Component({
-  selector: 'app-changepassword',
-  templateUrl: './changepassword.component.html',
-  styleUrls: ['./changepassword.component.css']
-})
-export class ChangepasswordComponent implements OnInit {
+@Component({ 
+  selector: 'app-changepassword', 
+  templateUrl: './changepassword.component.html', 
+  styleUrls: ['./changepassword.component.css'] 
+}) 
+export class ChangepasswordComponent implements OnInit { 
 
-  constructor(private snack: MatSnackBar,private userservice:UserService,private router:Router) { }
+  constructor(private snack: MatSnackBar,private userservice:UserService,private router:Router) { } 
 
-  user;
+  user; 
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { 
+  } 
 
-  cancel(){
-    this.router.navigate(['admin/users']);
+  cancel(){ 
+    this.router.navigate(['admin/users']); 
 
-  }
+  } 
   
-  formSubmit(){
+  formSubmit(){ 
     
     
-    this.userservice.updateUser(1,this.user).subscribe((data:any)=>
-    {
+    this.userservice.updateUser(1,this.user).subscribe((data:any)=> 
+    { 
       
-      Swal.fire('USERID:-'+data.id,'edited success','success');
-      this.router.navigate(['admin/users']);
+      Swal.fire('USERID:-'+data.id,'edited success','success'); 
+      this.router.navigate(['admin/users']); 
       
-    }),
-    (error) => {
-      console.log(error);
-      //alert('Something Wrong');
-      this.snack.open(error.error.message, '', {
-        duration: 5000,
-      });
-    }
-  }
+    }), 
+    (error) => { 
+      console.log(error); 
+      //alert('Something Wrong'); 
+      this.snack.open(error.error.message, '', { 
+        duration: 5000, 
+      }); 
+    } 
+  } 
 
  
 
-}
+} 
