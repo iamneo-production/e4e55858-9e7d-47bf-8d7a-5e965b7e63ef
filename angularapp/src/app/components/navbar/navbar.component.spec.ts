d463045aad5ed88d838fd78fec6e9241abbaf287
vijaya27022001 <99819290+vijaya27022001@users.noreply.github.com>
2022-03-31 12:26:15 +0530

    Update navbar.component.spec.ts

--- angularapp/src/app/components/navbar/navbar.component.spec.ts
+++ angularapp/src/app/components/navbar/navbar.component.spec.ts
@@ -1,25 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; 
 
-import { NavbarComponent } from './navbar.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+import { NavbarComponent } from './navbar.component'; 
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+describe('NavbarComponent', () => { 
+  
+  let component: NavbarComponent; 
+  
+  let fixture: ComponentFixture<NavbarComponent>; 
+  
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  beforeEach(async () => { 
+    
+    await TestBed.configureTestingModule({ 
+      
+      declarations: [ NavbarComponent ] 
+      
+    }) 
+    
+    .compileComponents(); 
+    
+  }); 
+  
+
+  beforeEach(() => { 
+      
+    fixture = TestBed.createComponent(NavbarComponent); 
+    
+    component = fixture.componentInstance; 
+    
+    fixture.detectChanges(); 
+    
+  }); 
+  
+
+  it('should create', () => { 
+    
+    expect(component).toBeTruthy(); 
+    
+  }); 
+  
+}); 
