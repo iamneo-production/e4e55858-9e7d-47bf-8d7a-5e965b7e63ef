a4c4cdb828beb52431d5202eaa8e8ba7d2e30756
Pranav-11-ctr <75959104+Pranav-11-ctr@users.noreply.github.com>
2022-03-31 12:41:57 +0530

    Update adminapprovedapplications.component.ts

--- angularapp/src/app/pages/admin/adminapprovedapplications/adminapprovedapplications.component.ts
+++ angularapp/src/app/pages/admin/adminapprovedapplications/adminapprovedapplications.component.ts
@@ -4,28 +4,28 @@ import { Subscription } from 'rxjs';
 import { SearchService } from 'src/app/search.service';
 import { AdmissionService } from 'src/app/services/admission.service';
 
-@Component({
-  selector: 'app-adminapprovedapplications',
-  templateUrl: './adminapprovedapplications.component.html',
-  styleUrls: ['./adminapprovedapplications.component.css']
-})
-export class AdminapprovedapplicationsComponent implements OnInit {
+@Component({ 
+  selector: 'app-adminapprovedapplications', 
+  templateUrl: './adminapprovedapplications.component.html', 
+  styleUrls: ['./adminapprovedapplications.component.css'] 
+}) 
+export class AdminapprovedapplicationsComponent implements OnInit { 
 
-  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { }
-  p:number=1;
-  admissions;
-  searchtext:any;
-  sub:Subscription;
-  ngOnInit(): void {
-    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);
-    this.getAllAdmissions();
-  }
-  getAllAdmissions(){
-    this.admissionservice.getallApprovedAdmissions().subscribe((data:any)=>{
-       this.admissions=data;
-    })
-  }
+  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { } 
+  p:number=1; 
+  admissions; 
+  searchtext:any; 
+  sub:Subscription; 
+  ngOnInit(): void { 
+    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data); 
+    this.getAllAdmissions(); 
+  } 
+  getAllAdmissions(){ 
+    this.admissionservice.getallApprovedAdmissions().subscribe((data:any)=>{ 
+       this.admissions=data; 
+    }) 
+  } 
 
   
 
-}
+} 
