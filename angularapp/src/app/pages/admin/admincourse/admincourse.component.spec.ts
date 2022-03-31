c590676e02a59d789b102c40e8e37aebfc61f1b9
Nishintha <70131750+Nishintha@users.noreply.github.com>
2022-03-31 12:28:35 +0530

    Update admincourse.component.spec.ts

--- angularapp/src/app/pages/admin/admincourse/admincourse.component.spec.ts
+++ angularapp/src/app/pages/admin/admincourse/admincourse.component.spec.ts
@@ -1,25 +1,44 @@
+//imports
+
+
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 
 import { AdmincourseComponent } from './admincourse.component';
 
+
 describe('AdmincourseComponent', () => {
+
   let component: AdmincourseComponent;
   let fixture: ComponentFixture<AdmincourseComponent>;
 
+  
   beforeEach(async () => {
+  
     await TestBed.configureTestingModule({
       declarations: [ AdmincourseComponent ]
+    
     })
-    .compileComponents();
+    
+      .compileComponents();
+  
   });
 
+  
   beforeEach(() => {
+  
     fixture = TestBed.createComponent(AdmincourseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  
   });
 
+  
   it('should create', () => {
+  
     expect(component).toBeTruthy();
+  
   });
+
 });
