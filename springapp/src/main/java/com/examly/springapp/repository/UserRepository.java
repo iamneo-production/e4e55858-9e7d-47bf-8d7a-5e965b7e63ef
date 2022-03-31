3139d71f8212e0223b169dc30487f5e507145bae
premkumarpaul <55930528+premkumarpaul@users.noreply.github.com>
2022-03-31 12:30:35 +0530

    Update UserRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -1,19 +1,20 @@
-package com.examly.springapp.repository;
+package com.examly.springapp.repository; 
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.data.jpa.repository.Query; 
+import org.springframework.data.repository.query.Param; 
 
-import com.examly.springapp.model.User;
+import com.examly.springapp.model.User;  
+ 
 
 
-
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> { 
+	 
+	public User findByUsername(String username); 
+	public User findByEmail(String email); 
+	public User findByMobileNumber(String number); 
 	
-	public User findByUsername(String username);
-	public User findByEmail(String email);
-	public User findByMobileNumber(String number);
+	@Query(value="select username from user where email=:mail",nativeQuery=true) 
+	public String findUsername(@Param("mail") String mail); 
 	
-	@Query(value="select username from user where email=:mail",nativeQuery=true)
-	public String findUsername(@Param("mail") String mail);
-}
+}  
