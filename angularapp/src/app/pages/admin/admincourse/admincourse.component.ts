c876a02244756ff30b7588e1461d4740e9fa2084
Nishintha <70131750+Nishintha@users.noreply.github.com>
2022-03-31 12:30:49 +0530

    Update admincourse.component.ts

--- angularapp/src/app/pages/admin/admincourse/admincourse.component.ts
+++ angularapp/src/app/pages/admin/admincourse/admincourse.component.ts
@@ -1,69 +1,112 @@
 import { Component, OnInit } from '@angular/core'; 
 
 import { Subscription } from 'rxjs'; 
+
 import { SearchService } from 'src/app/search.service';  
+
 import { CourseService } from 'src/app/services/course.service'; 
+
 import Swal from 'sweetalert2'; 
 
+
 @Component({
+
+  
   
   selector: 'app-admincourse',  
+  
   templateUrl: './admincourse.component.html',  
+  
   styleUrls: ['./admincourse.component.css'] 
+  
   
 }) 
  
+
 export class AdmincourseComponent implements OnInit {  
   
 
+
   constructor(private courseservice:CourseService,private searchservice:SearchService) { }  
   
+  
   courses;  
+  
   searchtext:any;  
+  
   sub:Subscription;  
- ngOnInit(): void {  
+ 
+  ngOnInit(): void {  
    
-  this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);  
+  
+    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);  
    
-   //Calling getCourse()  
-   this.getCourses(); 
    
- }  
+    //Calling getCourse()  
+   
+    this.getCourses(); 
+   
+ 
+  }  
 
+  
   //getCourse() to get all the course
- getCourses(){  
-   this.courseservice.getCourses().subscribe((data:any)=>{ 
+ 
+  getCourses(){  
+  
+    this.courseservice.getCourses().subscribe((data:any)=>{ 
+    
       this.courses=data; 
+      
       console.log(data); 
      
-   })  
    
- } 
+    })  
+   
+ 
+  } 
 
+  
   //delete course using courseId
- deleteCourse(courseId){ 
-   this.courseservice.deleteCourse(courseId)   
-   .subscribe((res:any)=> {  
+ 
+  deleteCourse(courseId){ 
+  
+    this.courseservice.deleteCourse(courseId)   
+   
+      .subscribe((res:any)=> {  
      
-     //Allert: Successfully deleted
-     Swal.fire('Successfully Deleted !!', res.message, 'success'); 
      
-     //calling ngOnInit to get the course list
-     this.ngOnInit(); 
+      //Allert: Successfully deleted
+     
+      Swal.fire('Successfully Deleted !!', res.message, 'success'); 
+     
+     
+      //calling ngOnInit to get the course list
+     
+      this.ngOnInit(); 
       
-}, 
+
+    }, 
               
 (error) => { 
   
-  console.log("mes"); 
-  console.log(error.message) 
-     
-  //alert('Something Wrong');
-  //deletion unsuccessful
-  Swal.fire('Deletion Unsuccess', error.error.message, 'error'); 
+
+      console.log("mes"); 
   
-}) 
+      console.log(error.message) 
+     
+  
+      //alert('Something Wrong');
+  
+      //deletion unsuccessful
+  
+      Swal.fire('Deletion Unsuccess', error.error.message, 'error'); 
+  
+
+    }) 
    
-}  
+
+  }  
  
+
 } 
