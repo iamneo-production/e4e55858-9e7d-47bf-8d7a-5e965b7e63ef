1a090657fee1a61e2e660047eccbbc137f467718
premkumarpaul <55930528+premkumarpaul@users.noreply.github.com>
2022-03-31 11:59:18 +0530

    Update login.service.spec.ts

--- angularapp/src/app/login.service.spec.ts
+++ angularapp/src/app/login.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'; 
 
-import { LoginService } from './login.service';
 
-describe('LoginService', () => {
-  let service: LoginService;
+import { LoginService } from './login.service'; 
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+
+describe('LoginService', () => { 
+  
+  let service: LoginService; 
+  
+
+  beforeEach(() => { 
+    
+    TestBed.configureTestingModule({}); 
+    
     service = TestBed.inject(LoginService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    
+  });  
+ 
+  it('should be created', () => {   
+    
+    expect(service).toBeTruthy();   
+    
+});  
