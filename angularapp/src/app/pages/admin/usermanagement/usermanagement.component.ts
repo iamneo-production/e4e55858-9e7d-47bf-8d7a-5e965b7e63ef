39e83a74de4e2e104828deedc13bdb2ac8afa66d
Pranav-11-ctr <75959104+Pranav-11-ctr@users.noreply.github.com>
2022-03-31 13:00:08 +0530

    Update usermanagement.component.ts

--- angularapp/src/app/pages/admin/usermanagement/usermanagement.component.ts
+++ angularapp/src/app/pages/admin/usermanagement/usermanagement.component.ts
@@ -4,67 +4,67 @@ import { SearchService } from 'src/app/search.service';
 import Swal from 'sweetalert2';
 import { UserService } from 'src/app/user.service';
 
-@Component({
-  selector: 'app-usermanagement',
-  templateUrl: './usermanagement.component.html',
-  styleUrls: ['./usermanagement.component.css']
-})
-export class UsermanagementComponent implements OnInit {
+@Component({ 
+  selector: 'app-usermanagement', 
+  templateUrl: './usermanagement.component.html', 
+  styleUrls: ['./usermanagement.component.css'] 
+}) 
+export class UsermanagementComponent implements OnInit { 
 
-  p:number=1;
-  searchtext:any;
-  users:any[];
-  user;
-  sub:Subscription;
-  constructor(private userservice:UserService,private searchservice:SearchService) { }
+  p:number=1; 
+  searchtext:any; 
+  users:any[]; 
+  user; 
+  sub:Subscription; 
+  constructor(private userservice:UserService,private searchservice:SearchService) { } 
 
-  ngOnInit(): void {
-    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);
-    this.getUsers();
+  ngOnInit(): void  { 
+    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data); 
+    this.getUsers(); 
 
-  }
+  } 
 
-  getUsers(){
-    this.userservice.getUsers().subscribe(data=>{
-      this.users=data;
-      console.log(this.users);
+  getUsers(){ 
+    this.userservice.getUsers().subscribe(data=>{ 
+      this.users=data; 
+      console.log(this.users); 
      
-    },
-    (error) => {
+    }, 
+    (error) => { 
       
-      Swal.fire((error.error.message),'error');
-      //alert('Something Wrong');
+      Swal.fire((error.error.message),'error'); 
+      //alert('Something Wrong'); 
 
-    })
-  }
-  deleteUser(id:number){
-    this.userservice.deleteUser(id).subscribe(
-     (data:any)=> {
-    Swal.fire((data.message),'successfull','success');
-     this.getUsers();
-    },
-    (error) => {
+    }) 
+  } 
+  deleteUser(id:number){ 
+    this.userservice.deleteUser(id).subscribe( 
+     (data:any)=> { 
+    Swal.fire((data.message),'successfull','success'); 
+     this.getUsers(); 
+    }, 
+    (error) => { 
       
-      Swal.fire((error.error.message),'error');
-      //alert('Something Wrong');
+      Swal.fire((error.error.message),'error'); 
+      //alert('Something Wrong'); 
 
-    }
-    );
+    } 
+    ); 
     
-  }
-  openUserdata(id:number){
+  } 
+  openUserdata(id:number){ 
     
-    this.userservice.getUserById(id).subscribe((data)=>{
-      this.user=data;
-      console.log(this.user);
-    },
-    (error) => {
+    this.userservice.getUserById(id).subscribe((data)=>{ 
+      this.user=data; 
+      console.log(this.user); 
+    }, 
+    (error) => { 
       
-      Swal.fire((error.error.message),'error');
-      //alert('Something Wrong');
+      Swal.fire((error.error.message),'error'); 
+      //alert('Something Wrong'); 
 
-    });
-  }
+    }); 
+  } 
 
 
-}
+} 
