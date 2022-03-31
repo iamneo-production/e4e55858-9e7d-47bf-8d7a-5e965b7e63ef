1c9066ca73006e4950789930f77ea66533cdfc3f
premkumarpaul <55930528+premkumarpaul@users.noreply.github.com>
2022-03-31 12:31:30 +0530

    Update RoleRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/RoleRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/RoleRepository.java
@@ -1,15 +1,18 @@
-package com.examly.springapp.repository;
+package com.examly.springapp.repository;  
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;  
+import org.springframework.data.jpa.repository.Query;  
+import org.springframework.data.repository.query.Param;  
 
-import com.examly.springapp.model.Role;
+import com.examly.springapp.model.Role;  
+ 
 
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
+public interface RoleRepository extends JpaRepository<Role,Integer> {  
 
 	
-	@Query(value="select *from role where role_name=:rolename",nativeQuery=true)
-	public Role roleExist(@Param("rolename") String rolename);
-}
+	@Query(value="select *from role where role_name=:rolename",nativeQuery=true)  
+	public Role roleExist(@Param("rolename") String rolename);  
+	
+	 
+}  
+   
