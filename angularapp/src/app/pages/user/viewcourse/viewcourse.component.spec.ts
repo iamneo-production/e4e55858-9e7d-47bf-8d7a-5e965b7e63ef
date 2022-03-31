4ddd0355b78770b326405e33fc08b2c81c907960
Nishintha <70131750+Nishintha@users.noreply.github.com>
2022-03-31 12:36:01 +0530

    Update viewcourse.component.spec.ts

--- angularapp/src/app/pages/user/viewcourse/viewcourse.component.spec.ts
+++ angularapp/src/app/pages/user/viewcourse/viewcourse.component.spec.ts
@@ -1,25 +1,45 @@
+//imports
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewcourseComponent } from './viewcourse.component';
 
+
 describe('ViewcourseComponent', () => {
+
   let component: ViewcourseComponent;
+  
   let fixture: ComponentFixture<ViewcourseComponent>;
 
+  
   beforeEach(async () => {
+  
     await TestBed.configureTestingModule({
+    
       declarations: [ ViewcourseComponent ]
+    
     })
-    .compileComponents();
+    
+      .compileComponents();
+  
   });
 
+  
   beforeEach(() => {
+  
     fixture = TestBed.createComponent(ViewcourseComponent);
+    
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
+  
   });
 
+  
   it('should create', () => {
+  
     expect(component).toBeTruthy();
+  
   });
+
 });
