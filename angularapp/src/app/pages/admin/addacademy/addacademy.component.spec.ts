8b5b80addada718538f5258543e9785e3c1fae39
vijaya27022001 <99819290+vijaya27022001@users.noreply.github.com>
2022-03-31 11:40:49 +0530

    Update addacademy.component.spec.ts

--- angularapp/src/app/pages/admin/addacademy/addacademy.component.spec.ts
+++ angularapp/src/app/pages/admin/addacademy/addacademy.component.spec.ts
@@ -1,25 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; 
 
-import { AddacademyComponent } from './addacademy.component';
+//import the required components
+
+import { AddacademyComponent } from './addacademy.component'; 
 
 describe('AddacademyComponent', () => {
-  let component: AddacademyComponent;
+  
+  let component: AddacademyComponent; 
+  
   let fixture: ComponentFixture<AddacademyComponent>;
+  
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddacademyComponent ]
-    })
+  beforeEach(async () => { 
+    
+    await TestBed.configureTestingModule({ 
+      
+      declarations: [ AddacademyComponent ] 
+      
+    }) 
+    
     .compileComponents();
-  });
+    
+  }); 
 
-  beforeEach(() => {
+  beforeEach(() => { 
+    
     fixture = TestBed.createComponent(AddacademyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    
+    component = fixture.componentInstance; 
+    
+    fixture.detectChanges(); 
+    
+  }); 
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should create', () => { 
+    
+    expect(component).toBeTruthy(); 
+    
+  }); 
+  
+}); 
