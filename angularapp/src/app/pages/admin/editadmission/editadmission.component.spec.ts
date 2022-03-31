99d5a10739964e7321571cb2b5b6a0b607fd114d
SHASHWAT KUMAR SHRIVASTAVA <69300179+Shashwat05-shrivastava@users.noreply.github.com>
2022-03-31 12:43:25 +0530

    Update editadmission.component.spec.ts

--- angularapp/src/app/pages/admin/editadmission/editadmission.component.spec.ts
+++ angularapp/src/app/pages/admin/editadmission/editadmission.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; 
 
-import { EditadmissionComponent } from './editadmission.component';
+import { EditadmissionComponent } from './editadmission.component'; 
 
-describe('EditadmissionComponent', () => {
-  let component: EditadmissionComponent;
-  let fixture: ComponentFixture<EditadmissionComponent>;
+describe('EditadmissionComponent', () => { 
+  let component: EditadmissionComponent; 
+  let fixture: ComponentFixture<EditadmissionComponent>; 
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditadmissionComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(async () => { 
+    await TestBed.configureTestingModule({ 
+      declarations: [ EditadmissionComponent ] 
+    }) 
+    .compileComponents(); 
+  }); 
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditadmissionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => { 
+    fixture = TestBed.createComponent(EditadmissionComponent); 
+    component = fixture.componentInstance; 
+    fixture.detectChanges(); 
+  }); 
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should create', () => { 
+    expect(component).toBeTruthy(); 
+  });  
+}); 
