511c587dd7a303730ad7197d7f332cceb027eb97
Pranav-11-ctr <75959104+Pranav-11-ctr@users.noreply.github.com>
2022-03-31 12:47:41 +0530

    Update adminstudent.component.ts

--- angularapp/src/app/pages/admin/adminstudent/adminstudent.component.ts
+++ angularapp/src/app/pages/admin/adminstudent/adminstudent.component.ts
@@ -4,28 +4,28 @@ import { Subscription } from 'rxjs';
 import { SearchService } from 'src/app/search.service';
 import { AdmissionService } from 'src/app/services/admission.service';
 
-@Component({
-  selector: 'app-adminstudent',
-  templateUrl: './adminstudent.component.html',
-  styleUrls: ['./adminstudent.component.css']
-})
-export class AdminstudentComponent implements OnInit {
-  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { }
+@Component({ 
+  selector: 'app-adminstudent', 
+  templateUrl: './adminstudent.component.html', 
+  styleUrls: ['./adminstudent.component.css'] 
+}) 
+export class AdminstudentComponent implements OnInit { 
+  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { } 
 
-  admissions;
-  searchtext:any;
-  p:number=1;
-  sub:Subscription;
-  ngOnInit(): void {
-    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);
-    this.getAllAdmissions();
-  }
-  getAllAdmissions(){
-    this.admissionservice.getallPendingAdmissions().subscribe((data:any)=>{
-       this.admissions=data;
-    })
-  }
+  admissions; 
+  searchtext:any; 
+  p:number=1; 
+  sub:Subscription; 
+  ngOnInit(): void { 
+    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data); 
+    this.getAllAdmissions(); 
+  } 
+  getAllAdmissions(){ 
+    this.admissionservice.getallPendingAdmissions().subscribe((data:any)=>{ 
+       this.admissions=data; 
+    }) 
+  } 
 
   
 
-}
+} 
