6614421cacabda9831b3285db5899434a5f881eb
Pranav-11-ctr <murarikmrmadhubani11@gmail.com>
2022-03-31 06:06:47 +0000

    review dashboard

--- angularapp/src/app/app.component.spec.ts
+++ angularapp/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import {MatSelectModule} from '@angular/material/select';
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        MatSelectModule
+        RouterTestingModule
       ],
       declarations: [
         AppComponent
---
+++ angularapp/src/app/review-dashboard/review-dash board.model.ts
@@ -0,0 +1,7 @@
+export class ReviewModel
+{
+    id:number = 0;
+    name:string = '';
+    rating:number = 0;
+    textarea:string = '';
+}
