8a79203333d2f461af9b3090d300d9c804f4bda2
Nishintha <70131750+Nishintha@users.noreply.github.com>
2022-03-31 12:23:52 +0530

    Update editcourse.component.spec.ts

--- angularapp/src/app/pages/admin/editcourse/editcourse.component.spec.ts
+++ angularapp/src/app/pages/admin/editcourse/editcourse.component.spec.ts
@@ -3,23 +3,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditcourseComponent } from './editcourse.component';
 
 describe('EditcourseComponent', () => {
+  
   let component: EditcourseComponent;
+  
   let fixture: ComponentFixture<EditcourseComponent>;
 
   beforeEach(async () => {
+  
     await TestBed.configureTestingModule({
+    
       declarations: [ EditcourseComponent ]
+    
     })
-    .compileComponents();
+    
+      .compileComponents();
+  
   });
 
+  
   beforeEach(() => {
+  
     fixture = TestBed.createComponent(EditcourseComponent);
+    
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
+  
   });
 
+  
   it('should create', () => {
+  
     expect(component).toBeTruthy();
+  
   });
+
 });
