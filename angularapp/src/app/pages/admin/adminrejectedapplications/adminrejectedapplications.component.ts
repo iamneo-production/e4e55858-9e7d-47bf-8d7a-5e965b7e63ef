cdb66cf55b6b4a881ead817f9a519b8aca301698
Pranav-11-ctr <75959104+Pranav-11-ctr@users.noreply.github.com>
2022-03-31 12:44:55 +0530

    Update adminrejectedapplications.component.ts

--- angularapp/src/app/pages/admin/adminrejectedapplications/adminrejectedapplications.component.ts
+++ angularapp/src/app/pages/admin/adminrejectedapplications/adminrejectedapplications.component.ts
@@ -5,29 +5,29 @@ import { Subscription } from 'rxjs';
 import { SearchService } from 'src/app/search.service';
 import { AdmissionService } from 'src/app/services/admission.service';
 
-@Component({
-  selector: 'app-adminrejectedapplications',
-  templateUrl: './adminrejectedapplications.component.html',
-  styleUrls: ['./adminrejectedapplications.component.css']
-})
-export class AdminrejectedapplicationsComponent implements OnInit {
+@Component({ 
+  selector: 'app-adminrejectedapplications', 
+  templateUrl: './adminrejectedapplications.component.html', 
+  styleUrls: ['./adminrejectedapplications.component.css'] 
+}) 
+export class AdminrejectedapplicationsComponent implements OnInit { 
 
   
-  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { }
-  p:number=1;
-  admissions;
-  searchtext:any;
-  sub:Subscription;
-  ngOnInit(): void {
-    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data);
-    this.getAllAdmissions();
-  }
-  getAllAdmissions(){
-    this.admissionservice.getallRejectedAdmissions().subscribe((data:any)=>{
-       this.admissions=data;
-    })
-  }
+  constructor(private admissionservice:AdmissionService,private router:Router,private searchservice:SearchService) { } 
+  p:number=1; 
+  admissions; 
+  searchtext:any; 
+  sub:Subscription; 
+  ngOnInit(): void { 
+    this.sub=this.searchservice.cm.subscribe(data=>this.searchtext=data); 
+    this.getAllAdmissions(); 
+  } 
+  getAllAdmissions(){ 
+    this.admissionservice.getallRejectedAdmissions().subscribe((data:any)=>{ 
+       this.admissions=data; 
+    }) 
+  } 
 
   
 
-}
+} 
